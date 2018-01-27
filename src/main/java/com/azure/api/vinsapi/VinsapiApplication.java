dae94768fc153f7577c9599dfcdede431ecda7f2
T1668A0 <T1668A0@CITMN5CG7221XYG.cag.cdroot.net>
2018-01-26 23:50:34 -0500

    commit 1

---
+++ src/main/java/com/azure/api/vinsapi/VinsapiApplication.java
@@ -0,0 +1,12 @@
+package com.azure.api.vinsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinsapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinsapiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/azure/api/vinsapi/VinsapiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.azure.api.vinsapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VinsapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
